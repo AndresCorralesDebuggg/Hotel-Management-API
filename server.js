d2f3a507362cedba557b96f9a1d18c0b54e58934
Andres Corrales <corralesandres165@gmail.com>
2026-04-30 15:13:19 -0600

    API funcionando: auth con bcrypt, clientes endpoint listo

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const express= require ('express');
 const {Pool} = require('pg');
 const cors = require('cors');
@@ -9,12 +12,61 @@ app.use(express.json());
 app.use(cors());
 
 const pool= new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'hotel_bd',
-    password: '12345678',
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
+
+
+
+//RUTAS Registro
+
+//Registro
+app.post('/registro', async (req,res)=>{
+    try {
+        const {usuario,contrasena}=req.body;
+        //Generar Hash de contraseña
+        const saltRounds = 10;//Decirle a bcrypt que ejecute 2^10 veces el algoritmo no paralelizable
+        const hashedPassword = await bcrypt.hash(contrasena, saltRounds);
+        //Guardar nuevo usuario en la BD
+        const result = await pool.query('INSERT INTO usuarios (usuario,contrasena) VALUES ($1, $2) RETURNING id_usuario, usuario',
+            [usuario, hashedPassword]
+        );
+        res.status(201).json({
+            mensaje: 'Usuario creado exitosamente',
+            usuario: result.rows[0]
+        });
+    } catch (error) {
+        if(error.code === '23505'){
+            return res.status(400).json({error: "El email ya está en uso por otro usuario"+error.message});
+        }
+        res.status(500).json({error:"No se ha podido registrar en usuario: "+error.message})
+    }
+});
+
+
+const verificarToken = (req,res,next)=> {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({error: "acceso denegado, token de autentificacion requerido"})
+    }
+    try {
+        const verificado = jwt.verify(token,process.env.JWT_SECRET);
+        req.user = verificado;
+        next();
+    } catch(error) {
+        res.status(401).json({error: "Token no validado correctly: "+ error.message});
+    }
+
+
+};
+
+
+//Rutas Tareas
 
 app.get('/clientes', async(req,res) =>{
     try{
@@ -70,7 +122,7 @@ app.get('/', async(req,res) => {
     }
 });
 
-app.post('/clientes', async (req,res) => {
+app.post('/clientes', verificarToken, async (req,res) => {
     try{
 
         const {nombre, direccion, telefono} = req.body;
@@ -128,21 +180,23 @@ app.put('/clientes/:id', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    try {
-        const { usuario, contrasena } = req.body;
-        const result = await pool.query(
-            'SELECT * FROM usuarios WHERE usuario = $1 AND contrasena = $2',
-            [usuario, contrasena]
-        );
+     try{
+            const {usuario,contrasena} = req.body;
+            const result = await pool.query('SELECT * FROM usuarios WHERE usuario = $1',[usuario]);
+            if (result.rows.length === 0){
+                return res.status(401).json({error: "Credenciales Incorrectas"});
+            }
+            const usuarioDB = result.rows[0];
+            const esValida = await bcrypt.compare(contrasena, usuarioDB.contrasena);
+            if (!esValida){
+                return res.status(401).json({error: "Credenciales incorrectas"})
+            }
 
-        if (result.rows.length > 0) {
-            res.json({ success: true, mensaje: "Login correcto" });
-        } else {
-            res.json({ success: false, mensaje: "Usuario o contraseña incorrectos" });
+            const token = jwt.sign({id: usuarioDB.id_usuario},process.env.JWT_SECRET, {expiresIn: '2h'});
+            res.json({token});
+        }catch(error) {
+            res.status(500).json({error:"No se pudo iniciar sesion: "+ error.message});
         }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
 });
 
 app.listen(3000, () => {
